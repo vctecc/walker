3c9480b4153c8b670676edc315c7f4dac918a98b
vctecc <vctecc@gmail.com>
2025-04-04 18:44:39 +0300

    refactoring

--- walker.py
+++ walker.py
@@ -1,5 +1,6 @@
 import osmnx as ox
 import os
+from networkx import MultiDiGraph
 import networkx as nx
 import random
 import folium
@@ -9,35 +10,31 @@ from geopy.geocoders import Nominatim
 
 GRAPH_FILE = "moscow_sao_graph.graphml"
 NODES_FILE = "valid_end_nodes.json"
+sao_boundary = "Северный административный округ, Москва, Россия"
+start_address = "Москва, Флотская 34к1"
 
-if os.path.exists(NODES_FILE):
-    with open(NODES_FILE) as file:
-        nodes = json.load(file)
-else:
-    if os.path.exists(GRAPH_FILE):
+
+def load_graph(graph_file: str, boundary: str) -> MultiDiGraph:
+    if os.path.exists(graph_file):
         print("Загружаем граф САО из файла...")
-        G = ox.load_graphml(GRAPH_FILE)
-    else:
-        print("Скачиваем граф для САО Москвы...")
-        sao_boundary = "Северный административный округ, Москва, Россия"
-        G = ox.graph_from_place(sao_boundary, network_type="walk")
-        ox.save_graphml(G, GRAPH_FILE)
-    nodes = list(G.nodes)
+        return ox.load_graphml(graph_file)
 
-geolocator = Nominatim(user_agent="route_generator")
-location = geolocator.geocode("Москва, Флотская 34к1")
-if location:
-    start_lat, start_lon = location.latitude, location.longitude
-    start_node = ox.nearest_nodes(G, start_lon, start_lat)
-else:
-    print("Ошибка: не удалось определить координаты адреса.")
-    exit()
+    print("Скачиваем граф для САО Москвы...")
+    graph = ox.graph_from_place(boundary, network_type="walk")
+    ox.save_graphml(graph, graph_file)
+    return graph
 
 
-def check_node(node):
+def get_location(address: str) -> tuple:
+    geolocator = Nominatim(user_agent="route_generator")
+    location = geolocator.geocode(address)
+    return location.latitude, location.longitude
+
+
+def check_node(node, graph, start_node):
     min_length = 1000  # 2 км
     max_length = 5000  # 5 км
-    dist = nx.shortest_path_length(G, start_node, node, weight="length")
+    dist = nx.shortest_path_length(graph, start_node, node, weight="length")
     return node if min_length <= dist <= max_length else None
 
 
@@ -61,30 +58,38 @@ def find_near_nodes():
     print(f"Найдено и сохранено {len(valid_end_nodes)} подходящих нод.")
 
 
-def cal_test(points: int):
+def create_route(points: int):
+    graph = load_graph(GRAPH_FILE, sao_boundary)
+    start_lon, start_lat = get_location(start_address)
+    if not start_lon:
+        print("Ошибка: не удалось определить координаты адреса.")
+        return
+
+    start_node = ox.nearest_nodes(graph, start_lon, start_lat)
     route = [start_node]
 
+    nodes = list(graph.nodes)
     waypoints = random.sample(nodes, points)
     for waypoint in waypoints:
-        segment = nx.shortest_path(G, route[-1], waypoint, weight="length")
+        segment = nx.shortest_path(graph, route[-1], waypoint, weight="length")
         route.extend(segment[1:])
 
-    route_back = nx.shortest_path(G, route[-1], start_node, weight="length")
+    route_back = nx.shortest_path(graph, route[-1], start_node, weight="length")
     route.extend(route_back[1:])
 
-    route_coords = [(G.nodes[node]['y'], G.nodes[node]['x']) for node in route]
+    route_coords = [(graph.nodes[node]['y'], graph.nodes[node]['x']) for node in route]
     m = folium.Map(location=route_coords[0], zoom_start=15)
 
     folium.PolyLine(route_coords, color="blue", weight=5, opacity=0.7).add_to(m)
 
-    start_coords = (G.nodes[start_node]['y'], G.nodes[start_node]['x'])
+    start_coords = (graph.nodes[start_node]['y'], graph.nodes[start_node]['x'])
     folium.Marker(start_coords, icon=folium.Icon(color="green")).add_to(m)
 
     end_node = waypoints[-1]
-    end_coord = (G.nodes[end_node]['y'], G.nodes[end_node]['x'])
+    end_coord = (graph.nodes[end_node]['y'], graph.nodes[end_node]['x'])
     folium.Marker(end_coord, icon=folium.Icon(color="red")).add_to(m)
 
-    coords = [(G.nodes[node]['y'], G.nodes[node]['x']) for node in waypoints[:-1]]
+    coords = [(graph.nodes[node]['y'], graph.nodes[node]['x']) for node in waypoints[:-1]]
     icon_color = "orange"
     for i, coord in enumerate(coords):
         folium.Marker(coord, icon=folium.Icon(color=icon_color)).add_to(m)
@@ -94,5 +99,5 @@ def cal_test(points: int):
 
 
 if __name__ == "__main__":
-    # cal_test(4)
-    find_near_nodes()
+    create_route(4)
+    # find_near_nodes()
